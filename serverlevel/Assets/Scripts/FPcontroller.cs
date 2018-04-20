85446c67598e8a4ed1926f2927bbc353e1a45b89
Timo <tpeters@sogyo.nl>
2018-04-20 17:10:14 +0200

    player added

---
+++ serverlevel/Assets/Scripts/FPcontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPcontroller : MonoBehaviour {
+
+    public float speed = 2f;
+    CharacterController player;
+
+    float moveFB;
+    float moveLR;
+
+	// Use this for initialization
+	void Start () {
+        player= GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        moveFB = 
+
+	}
+}
